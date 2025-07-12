300daaf7833a841972fe0e774c20e698476b5534
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-07-12 16:48:40 -0300

    feat: adds error handlers to index file

--- src/index.js
+++ src/index.js
@@ -1,19 +1,40 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
 // import router from "./routes/productRoutes.js";
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
+// Security Middlewares
 app.use(cors());
 // app.use("/api/products", router);
+
+// JSON Parsing
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send(`
     <h1>Hola mundo!</h1>
     <p>Visita la API de productos en este <a href="http://localhost:3000/api/products/">link</a>.</p>
     `);
+});
+
+// Handling routes not found (404)
+app.use((req, res, next) => {
+  res.status(404).json({
+    error: "Ruta no encontrada",
+  });
+});
+
+// Middleware for general error handling
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({
+    error: "Ocurrió un error en el servidor",
+  });
 });
 
 app.listen(PORT, () => {
