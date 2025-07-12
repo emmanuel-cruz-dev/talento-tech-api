8436d652afb24b421800fb73473a83e32cbdbb81
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-07-12 17:03:48 -0300

    feat: configures methods in product.route file

--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -2,4 +2,10 @@ import express from "express";
 
 const router = express.Router();
 
+router
+  .get("/", getProducts)
+  .get("/:productId", getProductById)
+  .post("/", createProduct)
+  .delete("/:id", deleteProduct);
+
 export default router;
