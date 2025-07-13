32a55de9ab855ae1deaa1c7f4d7c8003c6696fec
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-07-13 17:07:26 -0300

    refactor: removes unused db file and updates functions in product.model file

---
+++ src/models/product.model.js
@@ -0,0 +1,17 @@
+const getAll = async () => {
+  return null;
+};
+
+const getProductById = async (productId) => {
+  return null;
+};
+
+const createProduct = async (product) => {
+  return null;
+};
+
+const deleteProduct = async (productId) => {
+  return null;
+};
+
+export default { getAll, getProductById, createProduct, deleteProduct };
