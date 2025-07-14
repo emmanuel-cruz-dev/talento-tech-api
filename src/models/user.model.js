baaecde29f8e61b5de565eac97147b979dcad299
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-07-14 14:40:42 -0300

    feat: adds user.model to models folder

---
+++ src/models/user.model.js
@@ -0,0 +1,20 @@
+export const users = [
+  {
+    id: 1,
+    username: "admin",
+    password: "$2b$10$rH8Q8qJYVEKLQ7WCfmkrbeQXtNFRJ3N1YUxOEhZXKqhGiTv2dZLsK",
+    role: "admin",
+  },
+  {
+    id: 2,
+    username: "user1",
+    password: "$2b$10$rH8Q8qJYVEKLQ7WCfmkrbeQXtNFRJ3N1YUxOEhZXKqhGiTv2dZLsK",
+    role: "user",
+  },
+  {
+    id: 3,
+    username: "profesor",
+    password: "$2b$10$rH8Q8qJYVEKLQ7WCfmkrbeQXtNFRJ3N1YUxOEhZXKqhGiTv2dZLsK",
+    role: "teacher",
+  },
+];
