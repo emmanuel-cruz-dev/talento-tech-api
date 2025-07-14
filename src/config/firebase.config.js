6f7a5cdde9ccae77e2cc70168664af50647da2b7
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-07-14 10:51:37 -0300

    feat: adds productCollection to firebase.config file

--- src/config/firebase.config.js
+++ src/config/firebase.config.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getFirestore, collection } from "firebase/firestore";
 import { config } from "dotenv";
 
 config();
@@ -16,5 +16,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const productCollection = collection(db, "productos");
 
-export { db };
+export { db, productCollection };
