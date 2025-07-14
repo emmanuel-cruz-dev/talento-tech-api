a038659e80aaaf19b55d3b46f47ad46ae84ebeab
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-07-14 15:42:01 -0300

    feat: removes profesor object in users array, user.model file

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -11,10 +11,4 @@ export const users = [
     password: "$2b$10$.n6OlmPNwtlVNWjTOGkIkuMfY./doPfDLYs4BMhJrAinNJC9C6mtq",
     role: "user",
   },
-  {
-    id: 3,
-    username: "profesor",
-    password: "$2b$10$.n6OlmPNwtlVNWjTOGkIkuMfY./doPfDLYs4BMhJrAinNJC9C6mtq",
-    role: "teacher",
-  },
 ];
