f762d6bc9f586fc212777a6f375b7c307e6b13ee
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-07-14 14:26:39 -0300

    feat: adds try/catch block to login function in auth.controller file

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,35 @@
-const login = (req, res) => {
-  res.json({ message: "login" });
+import { config } from "dotenv";
+import jwt from "jsonwebtoken";
+import validateUser from "../services/auth.service.js";
+
+config();
+
+const login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    // Validate data
+    if (!username || !password) {
+      return res
+        .status(400)
+        .json({ error: "Username y password son requeridos" });
+    }
+
+    const userValid = await validateUser(username, password);
+
+    if (!userValid) {
+      return res.status(401).json({ error: "Credenciales inválidas" });
+    }
+
+    const token = jwt.sign({ username }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+
+    res.json({ token });
+  } catch (error) {
+    console.log("Error al loguearse:", error);
+    res.status(500).json({ error: "Error interno del servidor" });
+  }
 };
 
 export default { login };
