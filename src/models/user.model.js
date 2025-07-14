a8a667da74c5feeff519e0de9f35b64a3c72e63f
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-07-14 15:35:27 -0300

    fix: updates password in users array in user.model file

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -2,19 +2,19 @@ export const users = [
   {
     id: 1,
     username: "admin",
-    password: "$2b$10$rH8Q8qJYVEKLQ7WCfmkrbeQXtNFRJ3N1YUxOEhZXKqhGiTv2dZLsK",
+    password: "$2b$10$.n6OlmPNwtlVNWjTOGkIkuMfY./doPfDLYs4BMhJrAinNJC9C6mtq",
     role: "admin",
   },
   {
     id: 2,
     username: "user1",
-    password: "$2b$10$rH8Q8qJYVEKLQ7WCfmkrbeQXtNFRJ3N1YUxOEhZXKqhGiTv2dZLsK",
+    password: "$2b$10$.n6OlmPNwtlVNWjTOGkIkuMfY./doPfDLYs4BMhJrAinNJC9C6mtq",
     role: "user",
   },
   {
     id: 3,
     username: "profesor",
-    password: "$2b$10$rH8Q8qJYVEKLQ7WCfmkrbeQXtNFRJ3N1YUxOEhZXKqhGiTv2dZLsK",
+    password: "$2b$10$.n6OlmPNwtlVNWjTOGkIkuMfY./doPfDLYs4BMhJrAinNJC9C6mtq",
     role: "teacher",
   },
 ];
