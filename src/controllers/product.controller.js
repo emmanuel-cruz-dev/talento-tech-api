b809066dff86a91070a057673461ae672109e597
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-07-25 17:58:14 -0300

    refactor: removes coments in product.controller file

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -46,7 +46,6 @@ const createProduct = async (req, res) => {
   try {
     const { nombre, precio, categoria, disponible } = req.body;
 
-    // Validate data
     if (!nombre || !precio || !categoria) {
       return res.status(400).json({
         error: "Faltan campos requeridos: nombre, precio, categoria",
