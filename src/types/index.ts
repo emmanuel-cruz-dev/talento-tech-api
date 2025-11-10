34d77f7bdca75c2fd9b4bdae8059af9f6fe7eb03
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-11-10 11:44:59 -0300

    feat: adds ValidationMiddleware to src/types/index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -9,3 +9,5 @@ export type {
 export type { PaginationOptions, PaginatedResult } from "./pagination.types.ts";
 
 export type { User } from "./user.types.ts";
+
+export type { ValidationMiddleware } from "./validation.types.ts";
