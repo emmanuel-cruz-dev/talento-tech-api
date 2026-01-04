29caf917f79af9990d39b5ff71509e34711b6a18
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-01-04 15:13:03 -0300

    feat: modifies functions and imports in src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,12 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-import express from "express";
-import type { Request, Response, NextFunction } from "express";
-import cors from "cors";
-import bodyParser from "body-parser";
-import { join } from "node:path";
-import productRoutes from "./routes/product.routes.js";
-import authRoutes from "./routes/auth.routes.js";
+import app from "./app.js";
 
-const app = express();
 const PORT = process.env.PORT || 3000;
-
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(express.static(join(process.cwd(), "public")));
-app.get("/", (req: Request, res: Response) => {
-  res.sendFile(join(process.cwd(), "public", "home.html"));
-});
-
-app.use("/api/products", productRoutes);
-app.use("/auth", authRoutes);
-
-app.use((req: Request, res: Response) => {
-  res.status(404).json({
-    error: "Ruta no encontrada",
-    message: `No se encontró la ruta '${req.originalUrl}'`,
-  });
-});
-
-app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-  console.error(err.stack);
-  res.status(500).json({
-    error: "Ocurrió un error en el servidor",
-    message: err.message || "Algo salió mal",
-  });
-});
 
 if (process.env.VERCEL !== "1") {
   app.listen(PORT, () => {
     console.log(`🚀 Server listening on port http://localhost:${PORT}`);
   });
 }
-
-export default app;
