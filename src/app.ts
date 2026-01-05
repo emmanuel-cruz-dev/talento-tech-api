491b8e3c0c22bc062950d559f93909a0e6c41f04
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-01-05 08:11:17 -0300

    feat: adds apiv1Routes and removes comments in src/app.ts

--- src/app.ts
+++ src/app.ts
@@ -3,26 +3,21 @@ import type { Request, Response, NextFunction } from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import { join } from "node:path";
-import productRoutes from "./routes/product.routes.js";
-import authRoutes from "./routes/auth.routes.js";
+import apiv1Routes from "./routes/index.js";
 
 const app = express();
 
-// Middlewares
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(join(process.cwd(), "public")));
 
-// Routes
 app.get("/", (req: Request, res: Response) => {
   res.sendFile(join(process.cwd(), "public", "home.html"));
 });
 
-app.use("/api/products", productRoutes);
-app.use("/auth", authRoutes);
+app.use("/api/v1", apiv1Routes);
 
-// 404 Error handler
 app.use((req: Request, res: Response) => {
   res.status(404).json({
     error: "Ruta no encontrada",
@@ -30,7 +25,6 @@ app.use((req: Request, res: Response) => {
   });
 });
 
-// Global error handler
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   console.error(err.stack);
   res.status(500).json({
