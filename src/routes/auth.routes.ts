de6ba1aa55338249d3db2bc4300c3e80674f63dd
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-01-05 12:07:25 -0300

    feat: updates routes in src/routes/auth.routes.ts

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,8 +1,17 @@
 import { Router } from "express";
 import authController from "../controllers/auth.controller.js";
+import authenticateToken from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
+router.post("/register", authController.register);
 router.post("/login", authController.login);
+
+router.get("/profile", authenticateToken, authController.getProfile);
+router.post(
+  "/change-password",
+  authenticateToken,
+  authController.changePassword
+);
 
 export default router;
