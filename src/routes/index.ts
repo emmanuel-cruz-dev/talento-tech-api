71586263cdb85918c2608925270bcfaaac592d61
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-01-06 08:47:38 -0300

    feat: adds swaggerOptionst to src/routes/index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -14,12 +14,25 @@ router.get("/health", (req, res) => {
 router.use("/products", productRoutes);
 router.use("/auth", authRoutes);
 router.use("/users", userRoutes);
+
+const swaggerOptions = {
+  explorer: true,
+  customCss: ".swagger-ui .topbar { display: none }",
+  customSiteTitle: "Talento Tech API Docs",
+  swaggerOptions: {
+    url: "/api/v1/docs/swagger.json",
+  },
+};
+
+router.get("/docs/swagger.json", (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  res.send(swaggerSpec);
+});
+
 router.use(
   "/docs",
   swaggerUi.serve,
-  swaggerUi.setup(swaggerSpec, {
-    explorer: true,
-  })
+  swaggerUi.setup(swaggerSpec, swaggerOptions)
 );
 
 export default router;
