eccc6659180cc2db008f62ee976b11bb40c7a0ac
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-01-06 09:12:07 -0300

    feat: removes swaggerUiOptions in src/routes/index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -15,20 +15,14 @@ router.use("/products", productRoutes);
 router.use("/auth", authRoutes);
 router.use("/users", userRoutes);
 
-router.get("/docs/swagger.json", (req, res) => {
-  res.setHeader("Content-Type", "application/json");
-  res.send(swaggerSpec);
-});
-
-const swaggerUiOptions = {
-  explorer: true,
-  customSiteTitle: "Talento Tech API Docs",
-  swaggerOptions: {
-    url: "/api/v1/docs/swagger.json",
-  },
-};
-
-router.use("/docs", swaggerUi.serve);
-router.get("/docs", swaggerUi.setup(null, swaggerUiOptions));
+router.use(
+  "/docs",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerSpec, {
+    explorer: true,
+    customSiteTitle: "Talento Tech API Docs",
+    customCss: ".swagger-ui .topbar { display: none }",
+  })
+);
 
 export default router;
